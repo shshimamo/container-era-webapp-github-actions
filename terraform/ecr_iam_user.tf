6b58ac72d055c04799ddceaaeb8056ac57d92899
hiroya.shimamoto <shshimamo@gmail.com>
2020-01-04 21:22:31 +0900

    Change deploy user's policy

--- terraform/ecr_iam_user.tf
+++ terraform/ecr_iam_user.tf
@@ -6,17 +6,31 @@ resource "aws_iam_user" "github-actions-user" {
   }
 }
 
+# アクセスキーは一旦手動で作成する(Vault使うのがよさそう?)
+# (アクセスキーを手動で作成するとterraform destroyの前に削除しなければいけないみたい)
 #resource "aws_iam_access_key" "github-actions-user" {
 #  user = "${aws_iam_user.github-actions-user.name}"
 #}
 
-data "aws_iam_policy" "ecr_access_role_policy" {
-  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+# AWS管理のポリシーを使う場合
+#data "aws_iam_policy" "ecr_access_role_policy" {
+#  arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+#}
+#resource "aws_iam_user_policy" "github-actions-user" {
+#  name = "github-actions-user"
+#  user = aws_iam_user.github-actions-user.name
+#
+#  policy = data.aws_iam_policy.ecr_access_role_policy.policy
+#}
+
+resource "aws_iam_policy" "deploy" {
+  name = "github-actions-deploy"
+  path = "/"
+  description = "deploy by github-actions"
+  policy = file("aws_iam_policies/ecr_policy.json")
 }
 
-resource "aws_iam_user_policy" "github-actions-user" {
-  name = "github-actions-user"
-  user = "${aws_iam_user.github-actions-user.name}"
-
-  policy = data.aws_iam_policy.ecr_access_role_policy.policy
+resource "aws_iam_user_policy_attachment" "deploy-attach" {
+  user = aws_iam_user.github-actions-user.name
+  policy_arn = "${aws_iam_policy.deploy.arn}"
 }
